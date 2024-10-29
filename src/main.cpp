f545060eeeb1f0f581405d2ead6461326e7935cf
hkucs-makerlab <makelab@cs.hku.hk>
2024-10-29 16:00:41 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,36 @@
 
 #include "LegTest.hpp"
 
+#define L1J1Pin 5
+#define L1J2Pin 13
+#define L1J3Pin 12
 
-#define L1J1Pin 3
-#define L1J2Pin 11
-#define L1J3Pin 10
+#define L2J1Pin 10
+#define L2J2Pin 11
+#define L2J3Pin 7
 
-#define L2J1Pin 8
-#define L2J2Pin 9
-#define L2J3Pin 5
+#define L3J1Pin 9
+#define L3J2Pin 8
+#define L3J3Pin 6
 
-#define L3J1Pin 7
-#define L3J2Pin 6
-#define L3J3Pin 4
+#define L4J1Pin 4
+#define L4J2Pin 3
+#define L4J3Pin 2
 
-#define L4J1Pin 2
-#define L4J2Pin 1
-#define L4J3Pin 0
-
+#define SERVO_SETUP_PIN A0
 
 // Joints
-Joint L1J1(L1J1Pin);
-Joint L1J2(L1J2Pin);
-Joint L1J3(L1J3Pin);
+Joint L1J1(L1J1Pin,0,false);
+Joint L1J2(L1J2Pin,0,false);
+Joint L1J3(L1J3Pin,0,false);
 
 Joint L2J1(L2J1Pin,0,true);
 Joint L2J2(L2J2Pin,0,true);
 Joint L2J3(L2J3Pin,0,true);
 
-Joint L3J1(L3J1Pin,0);
-Joint L3J2(L3J2Pin,0);
-Joint L3J3(L3J3Pin,0);
+Joint L3J1(L3J1Pin,0,false);
+Joint L3J2(L3J2Pin,0,false);
+Joint L3J3(L3J3Pin,0,false);
 
 Joint L4J1(L4J1Pin, 0,true);
 Joint L4J2(L4J2Pin, 0,true);
@@ -44,8 +44,7 @@ Leg L2(2,L2J1, L2J2, L2J3);  // left front
 Leg L3(3,L3J1, L3J2, L3J3);  // right front
 Leg L4(4,L4J1, L4J2, L4J3);  // right rear
 
-
-LegTest legTest(L3);
+LegTest legTest(L1);
 
 void loop() {
   legTest.swing();
@@ -56,10 +55,15 @@ void loop() {
 void setup() {
 
   Serial.begin(115200);
-  Serial.println("[start]");
-
   legTest.setup();
-  while(1);
+  if (1) {
+    pinMode(SERVO_SETUP_PIN, INPUT);
+    while (digitalRead(SERVO_SETUP_PIN)) {
+      Console.println("[calibrate]");
+      delay(1000);
+    }
+  }
   legTest.stance();
   delay(2000);
+  Serial.println("[started]");
 }
